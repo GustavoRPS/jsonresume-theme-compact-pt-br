a7d8dc767752ee44babe7aa749775a78ec5d235b
GustavoRPS <email@gustavorps.net>
2016-12-13 20:13:52 -0200

    make a better print version

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ var Moment = require('moment');
 function render(resume) {
   var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+  var print = fs.readFileSync(__dirname + "/print.css", "utf-8");
 
 	return Handlebars.compile(template)({
 		css: css,
-		resume: resume
+    print: print,
+    resume: resume,
 	});
 };
 
